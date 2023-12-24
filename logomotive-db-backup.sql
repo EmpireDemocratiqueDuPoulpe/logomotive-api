285165541f003b87fe0fd54ed045bc47784ebd26
EmpireDemocratiqueDuPoulpe <alexislecomte.pro@protonmail.com>
2023-12-24 13:37:41 +0100

    README.md update

--- logomotive-db-backup.sql
+++ logomotive-db-backup.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2023-11-20 22:12:48
+-- Started on 2023-12-24 13:22:31
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,6 +17,40 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- TOC entry 220 (class 1255 OID 32800)
+-- Name: update_scripts_update_at(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_scripts_update_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_scripts_update_at() OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1255 OID 32801)
+-- Name: update_scripts_updated_at(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_scripts_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_scripts_updated_at() OWNER TO postgres;
 
 SET default_tablespace = '';
 
@@ -47,7 +81,8 @@ CREATE TABLE public.scripts (
     content text NOT NULL,
     tags text[],
     is_public boolean DEFAULT false NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
@@ -99,64 +134,79 @@ ALTER TABLE public.users ALTER COLUMN user_id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 4856 (class 0 OID 32788)
+-- TOC entry 4860 (class 0 OID 32788)
 -- Dependencies: 219
 -- Data for Name: script_sharing_links; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.script_sharing_links (link_id, script_id) VALUES ('4b01aaec-3506-403f-b527-4e441f9d2f60', 9);
 
 
 --
--- TOC entry 4855 (class 0 OID 24577)
+-- TOC entry 4859 (class 0 OID 24577)
 -- Dependencies: 218
 -- Data for Name: scripts; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.scripts (script_id, user_id, name, content, tags, is_public, created_at) OVERRIDING SYSTEM VALUE VALUES (9, 16, 'cacaz', 'AV 100
-TD 90
+INSERT INTO public.scripts (script_id, user_id, name, content, tags, is_public, created_at, updated_at) OVERRIDING SYSTEM VALUE VALUES (12, 16, 'test', 'AV 50', '{""}', false, '2023-12-04 22:08:44.696263+01', '2023-12-04 22:08:44.696263+01');
+INSERT INTO public.scripts (script_id, user_id, name, content, tags, is_public, created_at, updated_at) OVERRIDING SYSTEM VALUE VALUES (9, 16, 'Carré et étoile', 'POUR STAR :taille
+  REPETER 8 [AV :taille TD 135]
+FIN
+
+
+FCB 0 0 0
+FCC 0 255 0
+
 AV 100
 TD 90
 AV 100
 TD 90
 AV 100
 TD 90
-CT', NULL, false, '2023-11-18 20:57:22.68854+01');
-INSERT INTO public.scripts (script_id, user_id, name, content, tags, is_public, created_at) OVERRIDING SYSTEM VALUE VALUES (11, 16, 'nulle', 'AV 90
+AV 100
+TD 90
+
+LC
+AV 150
+BC
+STAR 150
+CT', '{géométrie,magnifique}', true, '2023-11-18 20:57:22.68854+01', '2023-12-24 13:15:44.467669+01');
+INSERT INTO public.scripts (script_id, user_id, name, content, tags, is_public, created_at, updated_at) OVERRIDING SYSTEM VALUE VALUES (11, 16, 'Premier script', 'AV 90
 RE 90
-AV 120', '{sex,caca,hihi}', true, '2023-11-18 20:57:22.68854+01');
+AV 120
+RE 10
+AV 1', '{"pas ouf","j''ai essayé","c''est rigolo"}', true, '2023-11-18 20:57:22.68854+01', '2023-12-24 13:16:48.681731+01');
 
 
 --
--- TOC entry 4852 (class 0 OID 16399)
+-- TOC entry 4856 (class 0 OID 16399)
 -- Dependencies: 215
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.users (user_id, username, email, password) OVERRIDING SYSTEM VALUE VALUES (15, 'mlamine', 'mo.lamine@sup.info', '$2b$10$BMywJxmN.S.8XVjnbmowx.01qd5QvBrrxdPzhm.u4l6gUBpEFsT5K');
 INSERT INTO public.users (user_id, username, email, password) OVERRIDING SYSTEM VALUE VALUES (16, 'Johmnny McNumgget', 'johmnny.mcnumgget@cheems.mail', '$2b$10$tVVunmI5nq3JnT.JAgHddeTExuHFPDBwXZZwC41L28w6IEdrcII/i');
+INSERT INTO public.users (user_id, username, email, password) OVERRIDING SYSTEM VALUE VALUES (15, 'mlamine', 'mo.lamine@sup.info', '"$2b$10$tVVunmI5nq3JnT.JAgHddeTExuHFPDBwXZZwC41L28w6IEdrcII/i"');
 
 
 --
--- TOC entry 4865 (class 0 OID 0)
+-- TOC entry 4869 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: scripts_script_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.scripts_script_id_seq', 11, true);
+SELECT pg_catalog.setval('public.scripts_script_id_seq', 12, true);
 
 
 --
--- TOC entry 4866 (class 0 OID 0)
+-- TOC entry 4870 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 16, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 17, true);
 
 
 --
--- TOC entry 4706 (class 2606 OID 32792)
+-- TOC entry 4709 (class 2606 OID 32792)
 -- Name: script_sharing_links script_sharing_links_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -165,7 +215,7 @@ ALTER TABLE ONLY public.script_sharing_links
 
 
 --
--- TOC entry 4704 (class 2606 OID 24583)
+-- TOC entry 4707 (class 2606 OID 24583)
 -- Name: scripts scripts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -174,7 +224,7 @@ ALTER TABLE ONLY public.scripts
 
 
 --
--- TOC entry 4702 (class 2606 OID 16405)
+-- TOC entry 4705 (class 2606 OID 16405)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -183,7 +233,15 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4708 (class 2606 OID 32793)
+-- TOC entry 4712 (class 2620 OID 32802)
+-- Name: scripts update_scripts_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER update_scripts_updated_at BEFORE UPDATE ON public.scripts FOR EACH ROW EXECUTE FUNCTION public.update_scripts_updated_at();
+
+
+--
+-- TOC entry 4711 (class 2606 OID 32793)
 -- Name: script_sharing_links SCRIPTS_SHARING_LINKS__SCRIPTS; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -192,7 +250,7 @@ ALTER TABLE ONLY public.script_sharing_links
 
 
 --
--- TOC entry 4707 (class 2606 OID 24584)
+-- TOC entry 4710 (class 2606 OID 24584)
 -- Name: scripts SCRIPT_USERS; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -201,7 +259,7 @@ ALTER TABLE ONLY public.scripts
 
 
 --
--- TOC entry 4862 (class 0 OID 0)
+-- TOC entry 4866 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: TABLE script_sharing_links; Type: ACL; Schema: public; Owner: postgres
 --
@@ -210,7 +268,7 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.script_sharing_links TO logo_u
 
 
 --
--- TOC entry 4863 (class 0 OID 0)
+-- TOC entry 4867 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: TABLE scripts; Type: ACL; Schema: public; Owner: postgres
 --
@@ -219,7 +277,7 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.scripts TO logo_user;
 
 
 --
--- TOC entry 4864 (class 0 OID 0)
+-- TOC entry 4868 (class 0 OID 0)
 -- Dependencies: 215
 -- Name: TABLE users; Type: ACL; Schema: public; Owner: postgres
 --
@@ -228,14 +286,14 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.users TO logo_user;
 
 
 --
--- TOC entry 2047 (class 826 OID 24589)
+-- TOC entry 2049 (class 826 OID 24589)
 -- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: -; Owner: postgres
 --
 
 ALTER DEFAULT PRIVILEGES FOR ROLE postgres GRANT SELECT,INSERT,DELETE,UPDATE ON TABLES TO logo_user;
 
 
--- Completed on 2023-11-20 22:12:48
+-- Completed on 2023-12-24 13:22:31
 
 --
 -- PostgreSQL database dump complete
