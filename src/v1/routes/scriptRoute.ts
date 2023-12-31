45d77192000ec6becdbc948a8002bc4a20728d91
EmpireDemocratiqueDuPoulpe <alexislecomte.pro@protonmail.com>
2023-12-31 11:38:26 +0100

    Swagger script route

--- src/v1/routes/scriptRoute.ts
+++ src/v1/routes/scriptRoute.ts
@@ -29,3 +29,127 @@ export default (router: Router) : void => {
 	 * DELETE
 	 *************************************************************/
 };
+
+/**
+ * @swagger
+ * tags:
+ *      - name: Script
+ *        description: The script domain routes.
+ * /api/v1/scripts:
+ *      post:
+ *          summary: Create a new script.
+ *          tags: [Script]
+ *          security:
+ *              - cookieAuth: []
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/NewScript"
+ *          responses:
+ *              200:
+ *                  description: The script is created.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: number
+ *                                  data:
+ *                                      type: object
+ *                                      properties:
+ *                                          script_id:
+ *                                              type: number
+ *      get:
+ *          summary: Get all scripts of a user.
+ *          tags: [Script]
+ *          security:
+ *              - cookieAuth: []
+ *          responses:
+ *              200:
+ *                  description: All script of the user are returned.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: number
+ *                                  data:
+ *                                      type: object
+ *                                      properties:
+ *                                          scripts:
+ *                                              type: array
+ *                                              items:
+ *                                                  $ref: "#/components/schemas/Script"
+ *      put:
+ *          summary: Update a script.
+ *          tags: [Script]
+ *          security:
+ *              - cookieAuth: []
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/UpdatingScript"
+ *          responses:
+ *              201:
+ *                  description: The script is updated.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: number
+ * /api/v1/scripts/{scriptID}:
+ *      get:
+ *          summary: Get a script by its id.
+ *          tags: [Script]
+ *          parameters:
+ *              - in: path
+ *                name: scriptID
+ *                description: The auto-generated script id.
+ *                schema:
+ *                  type: number
+ *                required: true
+ *          responses:
+ *              200:
+ *                  description: The script is returned.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: number
+ *                                  data:
+ *                                      type: object
+ *                                      properties:
+ *                                          script:
+ *                                              $ref: "#/components/schemas/Script"
+ * /api/v1/scripts/all:
+ *      get:
+ *          summary: Get all public scripts.
+ *          tags: [Script]
+ *          responses:
+ *              200:
+ *                  description: All public script are returned.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: number
+ *                                  data:
+ *                                      type: object
+ *                                      properties:
+ *                                          scripts:
+ *                                              type: array
+ *                                              items:
+ *                                                  $ref: "#/components/schemas/Script"
+ */
